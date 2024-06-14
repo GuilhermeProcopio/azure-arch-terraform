26353b16c3146438ce36e76f3d664a51fb8f01d2
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-06-14 18:19:24 -0300

    feat: adding rules on lb

--- main.tf
+++ main.tf
@@ -36,21 +36,80 @@ resource "azurerm_lb" "load_bancer" {
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "Standard"
 
-frontend_ip_configuration {
-  name                 = "deafult-frontend-ip"
-  public_ip_address_id = azurerm_public_ip.public_ip.id
-} 
+  frontend_ip_configuration {
+    name                 = "deafult-frontend-ip"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
 }
 
 resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
   loadbalancer_id = azurerm_lb.load_bancer.id
-  name = "lb-backendpool"
+  name            = "lb-backendpool"
 }
 
 resource "azurerm_lb_probe" "lb_probe" {
   loadbalancer_id = azurerm_lb.load_bancer.id
-  name = "lb-health-probe"
-  port = 80
-  protocol = "Tcp"
+  name            = "lb-health-probe"
+  port            = 80
+  protocol        = "Tcp"
 }
+
+resource "azurerm_lb_rule" "lb_rule" {
+  probe_id                       = azurerm_lb_probe.lb_probe.id
+  loadbalancer_id                = azurerm_lb.load_bancer.id
+  name                           = "httprule"
+  protocol                       = "tcp"
+  frontend_port                  = "80"
+  backend_port                   = "80"
+  frontend_ip_configuration_name = "deafult-frontend-ip"
+  disable_outbound_snat          = "true"
+  idle_timeout_in_minutes        = 15
+  enable_tcp_reset               = "true"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb_backend_pool.id]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "default-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "NSG-HTTP-RULE"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+
+  }
+}
+
+resource "azurerm" "name" {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
