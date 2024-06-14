3b3dbb3673e4f5f3ba7dc1b0d15e74b52fa7ea68
Guilherme Procopio <71650069+GuilhermeProcopio@users.noreply.github.com>
2024-06-14 16:27:41 -0300

    feat: adding basic variables
    Adding variables to manage better the terraform script with more modularity

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "resource_group_name_prefix" {
+  type        = string
+  default     = "rg"
+  description = "Prefix to combinate with a randomly name created"
+}
+
+variable "vm_user" {
+  type        = string
+  default     = "vm-user"
+  description = "User to auth on the VM"
+}
+
