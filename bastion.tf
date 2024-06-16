9fd80c24480725e01455eef402c0759928ce6b42
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-06-16 20:33:43 -0300

    fix: changing the right sku syntax

--- bastion.tf
+++ bastion.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "bastion_public_ip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
-  sku                 = "standard"
+  sku                 = "Standard"
   zones               = ["1", "2", "3"]
 }
 
