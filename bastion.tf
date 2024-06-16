13c21ce21e9623e6e4d260c8d2e2625ca92e6759
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-06-16 20:26:33 -0300

    fix: formated tf file

--- bastion.tf
+++ bastion.tf
@@ -1,29 +1,29 @@
 resource "azurerm_public_ip" "bastion_public_ip" {
-  name = "bastion-public-ip"
-  location = azurerm_resource_group.rg.location
+  name                = "bastion-public-ip"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method = "Static"
-  sku = "standard"
-  zones = ["1", "2", "3"]
+  allocation_method   = "Static"
+  sku                 = "standard"
+  zones               = ["1", "2", "3"]
 }
 
 resource "azurerm_subnet" "bastion_subnet" {
-  name = "bastion-subnet"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                 = "bastion-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = ["10.1.1.0/27"]
+  address_prefixes     = ["10.1.1.0/27"]
 }
 
 resource "azurerm_bastion_host" "bastion_host" {
-    name = "bastion-host"
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    sku = "Standard"
-    tunneling_enabled = "true"
+  name                = "bastion-host"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  tunneling_enabled   = "true"
 
-    ip_configuration {
-      name = "bastion-ipconfig"
-      subnet_id = azurerm_subnet.bastion_subnet.id
-      public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
-    }
+  ip_configuration {
+    name                 = "bastion-ipconfig"
+    subnet_id            = azurerm_subnet.bastion_subnet.id
+    public_ip_address_id = azurerm_public_ip.bastion_public_ip.id
+  }
 }
