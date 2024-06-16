00895fc29e2f258b67c99eb409d8ce052a11d1da
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-06-16 20:35:33 -0300

    fix: changing the resource_id for the right resource

--- ssh.tf
+++ ssh.tf
@@ -5,7 +5,7 @@ resource "random_pet" "ssh_key_name" {
 
 resource "azapi_resource_action" "ssh_public_key_gen" {
   type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource_ssh.ssh_public_key_gen.id
+  resource_id = azapi_resource.ssh_public_key.id
   action      = "generateKeyPair"
   method      = "POST"
 
