a1b09c81abc674ff8764f127bcecd27c4de192f2
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-06-16 19:46:48 -0300

    feat: adding gateway configurations

---
+++ gateway.tf
@@ -0,0 +1,26 @@
+resource "azurerm_public_ip" "nat_gateway_public_ip" {
+  name                = "nat-gateway-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1", "2", "3"]
+}
+
+resource "azurerm_nat_gateway" "nat_gateway" {
+  name                    = "gateway"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "gateway_association" {
+    nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
+    public_ip_address_id = azurerm_public_ip.nat_gateway_public_ip.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "gateway_subnet" {
+  subnet_id = azurerm_subnet.subnet.id
+  nat_gateway_id = azurerm_nat_gateway.nat_gateway.id
+}
