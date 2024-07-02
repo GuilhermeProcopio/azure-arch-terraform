d3cb4d37b20f03aed9262f1b3a2dc12aae2ebf37
GuilhermeProcopio <guilhermecprocopio@gmail.com>
2024-07-02 19:57:48 -0300

    feat! Adding count on files

--- backendservers.tf
+++ backendservers.tf
@@ -1,47 +1,60 @@
-resource "azurerm_network_interface" "vm1_nic" {
-  name                = "nic-vm1"
+resource "azurerm_network_interface" "vm_nic" {
+  count               = 2
+  name                = "nic-vm-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "nic1-config"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface" "vm2_nic" {
-  name                = "nic-vm2"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "nic2_config"
+    name                          = "nic-${count.index}-config" # O nome da configuração de IP é "nic1-config"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 # Address pool
-resource "azurerm_network_interface_backend_address_pool_association" "nic1_address_pool" {
-  network_interface_id    = azurerm_network_interface.vm1_nic.id
-  ip_configuration_name   = "nic1-address-pool"
+resource "azurerm_network_interface_backend_address_pool_association" "nic_address_pool" {
+  count                   = 2
+  network_interface_id    = azurerm_network_interface.vm_nic[count.index].id
+  ip_configuration_name   = "nic-${count.index}-config"
   backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_pool.id
 }
 
-resource "azurerm_network_interface_backend_address_pool_association" "nic2_address_pool" {
-  network_interface_id    = azurerm_network_interface.vm2_nic.id
-  ip_configuration_name   = "nic2-address-pool"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_pool.id
-}
-
-resource "azurerm_network_interface_security_group_association" "nsg_nic1" {
-  network_interface_id = azurerm_network_interface.vm1_nic.id
+resource "azurerm_network_interface_security_group_association" "nsg_nic" {
+  count                     = 2
+  network_interface_id      = azurerm_network_interface.vm_nic[count.index].id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-resource "azurerm_network_interface_security_group_association" "nsg_nic2" {
-  network_interface_id = azurerm_network_interface.vm2_nic.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
+resource "azurerm_linux_virtual_machine" "vm" {
+  count                 = 2
+  name                  = "linux-vm-${count.index}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.vm_nic[count.index].id]
+  size                  = "Standard_DS1_v2"
 
+  os_disk {
+    name                 = "vm-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name  = "vm-${count.index}"
+  admin_username = var.vm_user
+
+  admin_ssh_key {
+    username   = var.vm_user
+    public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.vm1_storage_account.primary_blob_endpoint
+  }
+}
--- virtualmachines.tf
+++ virtualmachines.tf
@@ -1,68 +1,56 @@
-resource "azurerm_linux_virtual_machine" "vm1" {
-  name                  = "linux-vm1"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.vm1_nic.id]
-  size                  = "Standard_DS1_v2"
+# resource "azurerm_network_interface" "vm2_nic" {
+#   name                = "nic-vm2"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  os_disk {
-    name                 = "vm1-disk"
-    caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
-  }
+#   ip_configuration {
+#     name                          = "nic2-config"  # O nome da configuração de IP é "nic2-config"
+#     subnet_id                     = azurerm_subnet.subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04_lts_gen2"
-    version   = "latest"
-  }
+# # Address pool
+# resource "azurerm_network_interface_backend_address_pool_association" "nic2_address_pool" {
+#   network_interface_id    = azurerm_network_interface.vm2_nic.id
+#   ip_configuration_name   = "nic2-config"  # Corrigido para "nic2-config"
+#   backend_address_pool_id = azurerm_lb_backend_address_pool.lb_backend_pool.id
+# }
 
-  computer_name  = "vm1"
-  admin_username = var.vm_user
+# resource "azurerm_network_interface_security_group_association" "nsg_nic2" {
+#   network_interface_id      = azurerm_network_interface.vm2_nic.id
+#   network_security_group_id = azurerm_network_security_group.nsg.id
+# }
 
-  admin_ssh_key {
-    username   = var.vm_user
-    public_key = azapi_resource_action.ssh_public_key_gen.output.public_key
-  }
+# resource "azurerm_linux_virtual_machine" "vm2" {
+#   name                  = "linux-vm2"
+#   location              = azurerm_resource_group.rg.location
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   network_interface_ids = [azurerm_network_interface.vm2_nic.id]
+#   size                  = "Standard_DS1_v2"
 
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.vm1_storage_account.primary_blob_endpoint
-  }
-}
+#   os_disk {
+#     name                 = "vm2-disk"
+#     caching              = "ReadWrite"
+#     storage_account_type = "StandardSSD_LRS"
+#   }
 
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts-gen2"
+#     version   = "latest"
+#   }
 
-# creates VM2
+#   computer_name  = "vm2"
+#   admin_username = var.vm_user
 
-resource "azurerm_linux_virtual_machine" "vm2" {
-  name                  = "linux-vm2"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.vm1_nic.id]
-  size                  = "Standard_DS1_v2"
+#   admin_ssh_key {
+#     username   = var.vm_user
+#     public_key = azapi_resource_action.ssh_public_key_gen.output.publicKey
+#   }
 
-  os_disk {
-    name                 = "vm2-disk"
-    caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04_lts_gen2"
-    version   = "latest"
-  }
-
-  computer_name  = "vm2"
-  admin_username = var.vm_user
-
-  admin_ssh_key {
-    username   = var.vm_user
-    public_key = azapi_resource_action.ssh_public_key_gen.output.public_key
-  }
-
-  boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.vm2_storage_account.primary_blob_endpoint
-  }
-}
+#   boot_diagnostics {
+#     storage_account_uri = azurerm_storage_account.vm2_storage_account.primary_blob_endpoint
+#   }
+# }
